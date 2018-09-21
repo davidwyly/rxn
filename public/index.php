e6a66dabcfa2e3c339c2ff5d887c89ebfdf76077
David Wyly <david.wyly@gmail.com>
2018-09-21 16:37:20 -0600

    rxn root is now vendor

--- public/index.php
+++ public/index.php
@@ -4,7 +4,7 @@ namespace Rxn\Framework;
 
 define(__NAMESPACE__ . '\START', microtime(true));
 define(__NAMESPACE__ . '\ROOT', __DIR__ . '/../');
-define(__NAMESPACE__ . '\RXN_ROOT', ROOT . 'src/');
+define(__NAMESPACE__ . '\RXN_ROOT', ROOT . 'vendor/davidwyly/rxn/src');
 define(__NAMESPACE__ . '\APP_ROOT', ROOT . 'app/');
 
 require_once(RXN_ROOT . '/Service.php');
