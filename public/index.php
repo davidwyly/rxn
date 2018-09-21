5c4fce20607db54cf5dae181399a6b22e1e667dd
David Wyly <david.wyly@gmail.com>
2018-09-21 17:11:26 -0600

    testing

--- public/index.php
+++ public/index.php
@@ -1,21 +1,12 @@
 <?php
 
-namespace Rxn\Framework;
-
 require_once(__DIR__ . '/../vendor/autoload.php');
 
 define(__NAMESPACE__ . '\START', microtime(true));
 define(__NAMESPACE__ . '\ROOT', __DIR__ . '/../');
-define(__NAMESPACE__ . '\RXN_ROOT', ROOT . 'vendor/davidwyly/rxn/src');
-define(__NAMESPACE__ . '\APP_ROOT', ROOT . 'app/');
-
-require_once(RXN_ROOT . '/Service.php');
-require_once(RXN_ROOT . '/BaseConfig.php');
-require_once(ROOT . '/config/Config.php');
-require_once(RXN_ROOT . '/Autoload.php');
 
 try {
-    $config = new Config();
+    $config = new Rxn\Framework\BaseConfig();
     new Autoload($config);
     $app = new App($config, new Datasources(), new Container());
 } catch (AppException $exception) {
