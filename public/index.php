f61e69595c92f9e611e4773271be82ffaf7bae8a
David James Wyly <david.wyly@gmail.com>
2018-09-21 20:25:15 -0600

    fixing

--- public/index.php
+++ public/index.php
@@ -1,15 +1,15 @@
-<?php
+<?php declare(strict_types=1);
 
 require_once(__DIR__ . '/../vendor/autoload.php');
-require_once(__DIR__ . '/../app/autoload.php');
+require_once(__DIR__ . '/../app/Autoloader.php');
 
 define(__NAMESPACE__ . '\START', microtime(true));
 define(__NAMESPACE__ . '\ROOT', __DIR__ . '/../');
 
 try {
-    $config = new Rxn\Framework\BaseConfig();
-    $app = new Rxn\Framework\App($config, new Datasource(), new Container());
-} catch (AppException $exception) {
+    $config = new Config();
+    $app = new App($config, new BaseDatasource(), new Container());
+} catch (Error\AppException $exception) {
     /** @noinspection PhpUnhandledExceptionInspection */
     App::renderEnvironmentErrors($exception);
     die();
