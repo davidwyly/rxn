d77e1d93950cfc29ca71440624bbbd74438cb5cc
David James Wyly <david.wyly@gmail.com>
2018-09-22 01:18:18 -0600

    more progress

--- app/Config/bootstrap.php
+++ app/Config/bootstrap.php
@@ -4,4 +4,12 @@
  * Bootstrap file for RXN Application
  */
 
-define('APP_NAMESPACE', getenv('APP_NAMESPACE'));
+define('Rxn\\Framework\\ENVIRONMENT', getenv('Rxn\\Framework\\ENVIRONMENT'));
+define('Rxn\\Framework\\APP_NAMESPACE', getenv('Rxn\\Framework\\APP_NAMESPACE'));
+define('Rxn\\Framework\\APP_TIMEZONE', getenv('Rxn\\Framework\\APP_TIMEZONE'));
+define('Rxn\\Framework\\APP_MULTIBYTE', getenv('Rxn\\Framework\\APP_MULTIBYTE'));
+
+define('Rxn\\Framework\\TEST_OAUTH_CONSUMER_KEY', getenv('Rxn\\Framework\\TEST_OAUTH_CONSUMER_KEY'));
+define('Rxn\\Framework\\TEST_OAUTH_CONSUMER_SECRET', getenv('Rxn\\Framework\\TEST_OAUTH_CONSUMER_SECRET'));
+define('Rxn\\Framework\\TEST_OAUTH_TOKEN_KEY', getenv('Rxn\\Framework\\TEST_OAUTH_TOKEN_KEY'));
+define('Rxn\\Framework\\TEST_OAUTH_TOKEN_SECRET', getenv('Rxn\\Framework\\TEST_OAUTH_TOKEN_SECRET'));
