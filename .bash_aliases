e5be166a59cd2ff8a3cebf5d9f3909f68f0b4510
David James Wyly <david.wyly@gmail.com>
2018-09-27 23:59:07 -0600

    got tests passing

--- .bash_aliases
+++ .bash_aliases
@@ -1,1 +1,2 @@
-alias rxn_tests="./vendor/bin/phpunit --bootstrap vendor/autoload.php src/Rxn/Orm/Tests/*"
+alias framework_test="./vendor/bin/phpunit --bootstrap vendor/autoload.php serc/Rxn/Framework/Tests/*"
+alias orm_tests="./vendor/bin/phpunit --bootstrap vendor/autoload.php src/Rxn/Orm/Tests/*"
--- src/Rxn/Orm/Tests/Builder/Query/JoinTest.php
+++ src/Rxn/Orm/Tests/Builder/Query/JoinTest.php
@@ -13,16 +13,25 @@ final class JoinTest extends TestCase
         $query->select(['users.id' => 'user_id'])
               ->from('users', 'u')
               ->join('orders', 'orders.user_id', '=', 'users.id', 'o')
-              ->where('users.id','=',12345)
               ->parseCommandAliases();
+
+        $expected_table_aliases = [
+            'users' => 'u',
+            'orders' => 'o',
+        ];
+        $this->assertEquals($expected_table_aliases, $query->table_aliases);
+
         $this->assertEquals('`u`.`id` AS `user_id`', $query->commands['SELECT'][0]);
+
         $this->assertEquals('`users` AS `u`', $query->commands['FROM'][0]);
-        $expected = [
+        $expected_join = [
             'orders' => [
                 'AS' => ['`o`'],
                 'ON' => ['`o`.`user_id` = `u`.`id`'],
             ],
         ];
-        $this->assertEquals($expected, $query->commands['INNER JOIN']);
+
+        $this->assertEquals($expected_join, $query->commands['INNER JOIN']);
+
     }
 }
